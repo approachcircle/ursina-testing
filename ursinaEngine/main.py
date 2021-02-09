a2e71230ffa8f91727ba4441a4487f90a80e0e59
approachcircle <58401347+approachcircle@users.noreply.github.com>
2021-02-09 03:14:14 +0000

    switched key order
    have switched the key press order in the elif statements to fit the more streamlined pronounciation of 'wasd'

--- ursinaEngine/main.py
+++ ursinaEngine/main.py
@@ -1,14 +1,14 @@
 from ursina import *
 
 def update():
-    if held_keys['a']:
+    if held_keys['w']:
+        cam.y += 1 * time.dt
+    elif held_keys['a']:
         cam.x -= 1 * time.dt
-    elif held_keys['d']:
-        cam.x += 1 * time.dt
     elif held_keys['s']:
         cam.y -= 1 * time.dt
-    elif held_keys['w']:
-        cam.y += 1 * time.dt
+    elif held_keys['d']:
+        cam.x += 1 * time.dt
 
 app = Ursina()
 approLogo_texture = load_texture('textures/approachcircle.bmp')
