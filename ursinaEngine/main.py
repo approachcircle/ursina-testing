ee3d43ae7786262a4ea34206794a2296644812e3
approachcircle <osc-mcd@protonmail.com>
2021-02-09 18:23:05 +0000

    fixed incorrect display of logo on standardised monitors

--- ursinaEngine/main.py
+++ ursinaEngine/main.py
@@ -13,6 +13,6 @@ def update():
 app = Ursina()
 approLogo_texture = load_texture('textures/approachcircle.bmp')
 cam_texture = load_texture('textures/cam.png')
-approLogo = Entity(model ='quad', scale = (4.8,0.5), position = (-4.86,-4.3), texture = approLogo_texture)
+approLogo = Entity(model ='quad', scale = (4.8,0.5), position = (-4.87,-3.838), texture = approLogo_texture)
 cam = Entity(model ='quad', scale = (7,7), position = (0,0), texture = cam_texture)
 app.run()
