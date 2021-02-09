8b6663dc460c9f82289a9e2ad224176f02467017
approachcircle <58401347+approachcircle@users.noreply.github.com>
2021-02-09 02:58:21 +0000

    Add files via upload

---
+++ ursinaEngine/main.py
@@ -0,0 +1,18 @@
+from ursina import *
+
+def update():
+    if held_keys['a']:
+        cam.x -= 1 * time.dt
+    elif held_keys['d']:
+        cam.x += 1 * time.dt
+    elif held_keys['s']:
+        cam.y -= 1 * time.dt
+    elif held_keys['w']:
+        cam.y += 1 * time.dt
+
+app = Ursina()
+approLogo_texture = load_texture('textures/approachcircle.bmp')
+cam_texture = load_texture('textures/cam.png')
+approLogo = Entity(model ='quad', scale = (4.8,0.5), position = (-4.86,-4.3), texture = approLogo_texture)
+cam = Entity(model ='quad', scale = (7,7), position = (0,0), texture = cam_texture)
+app.run()
