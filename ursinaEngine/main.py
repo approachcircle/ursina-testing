94c04b21518f46cb593ec3d2d191355228f0ddf9
approachcircle <osc-mcd@protonmail.com>
2021-02-10 23:24:26 +0000

    changed one line space to two lines

--- ursinaEngine/main.py
+++ ursinaEngine/main.py
@@ -1,5 +1,6 @@
 from ursina import *
 
+
 def update():
     if held_keys['w']:
         cam.y += 1 * time.dt
@@ -10,9 +11,10 @@ def update():
     elif held_keys['d']:
         cam.x += 1 * time.dt
 
+
 app = Ursina()
 approLogo_texture = load_texture('textures/approachcircle.bmp')
 cam_texture = load_texture('textures/cam.png')
-approLogo = Entity(model ='quad', scale = (4.8,0.5), position = (-4.87,-3.838), texture = approLogo_texture)
-cam = Entity(model ='quad', scale = (7,7), position = (0,0), texture = cam_texture)
+approLogo = Entity(model='quad', scale=(4.8, 0.5), position=(-4.87, -3.838), texture=approLogo_texture)
+cam = Entity(model='quad', scale=(7, 7), position=(0, 0), texture=cam_texture)
 app.run()
